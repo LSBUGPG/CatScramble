6905e47683cbc962c90f7ac1cbbd961c46aaf806
Paul Sinnett <paul.sinnett@gmail.com>
2024-11-26 17:38:27 +0000

    see target

--- Assets/MoveToTarget.cs
+++ Assets/MoveToTarget.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class MoveToTarget : MonoBehaviour
 {
     public Transform target;
+    public float maxDistance = 10;
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    bool CanSeeTarget()
+    {
+        Ray toTarget = new(transform.position, target.position);
+        return !Physics.Raycast(toTarget, maxDistance);
+    }
+
     void Update()
     {
         float distance = Vector3.Distance(transform.position, target.position);
-        if (distance < 10 && target.position.y < 0.1f)
+        if (CanSeeTarget())
         {
             transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime);        
         }
