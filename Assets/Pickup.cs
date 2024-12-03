de1055a873d7d2024bba4ef1feaad41c931c0f0a
Paul Sinnett <paul.sinnett@gmail.com>
2024-12-03 16:08:43 +0000

    added coin pickup

---
+++ Assets/Pickup.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public float speed = 360;
+    public AudioClip clink;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
+        //Player player = other.GetComponent<Player>();
+        //if (player != null)
+        //{
+        //    AudioSource.PlayClipAtPoint(clink, Camera.main.transform.position);
+        //    player.AddCoin();
+        //    gameObject.SetActive(false);
+        //}
+    }
+}
